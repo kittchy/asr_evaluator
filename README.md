f8eb8374633592fc4df538b609f48e72ae1a8551
kinouchi takahiro <drums.radwimps203@gmail.com>
2022-09-29 08:46:21 +0900

    [FIX] sagemaker依存のスクリプトから一般的なものに修正

--- README.md
+++ README.md
@@ -1,62 +1,60 @@
-# ESPnetのEvaluationのみを行う
+# ESPnet の Evaluation のみを行う
 
-Sagemakerの推論ステップで推論とCERの評価部分だけの実装を行ったので、使いやすいようにドキュメントとしてまとめておきます。
+Sagemaker の推論ステップで推論と CER の評価部分だけの実装を行ったので、使いやすいようにドキュメントとしてまとめておきます。
 
-## Inference
+## Inference & scorering
 
-1. ESPnet のpython環境構築
+1. ESPnet の python 環境構築
 
-  ESPnetの[Install](https://espnet.github.io/espnet/installation.html)をもとに環境を作成してください。以下のコマンドを叩いてみて、argumentsが表示されればOKです。
+ESPnet の[Install](https://espnet.github.io/espnet/installation.html)をもとに環境を作成してください。以下のコマンドを叩いてみて、arguments が表示されれば OK です。
 
 ```
 python -c "from espnet2.bin.asr_inference import inference, get_parser"
 ```
-3.  CERの計算の実行環境構築
 
-  SCTKをダウンロードします。
-  ```
-  # SCTK (sclite: CERなどの計算に必要)
-    cd <任意のパス>
-    git clone https://github.com/usnistgov/SCTK.git \
-    && cd SCTK \
-    && make config \
-    && make all \
-    && make check \
-    && make install \
-    && make doc
+2.  CER の計算の実行環境構築
 
-    PATH=$PATH:<任意のパス>/SCTK/bin
-  ```
-  
-  以下のコマンドを使ってArgumentが出力されればOKです
+SCTK をダウンロードします。
 
-  ```bash
-  sclite
-  ```
-	
-2. スクリプト
+```
+# SCTK (sclite: CERなどの計算に必要)
+  cd <任意のパス>
+  git clone https://github.com/usnistgov/SCTK.git \
+  && cd SCTK \
+  && make config \
+  && make all \
+  && make check \
+  && make install \
+  && make doc
 
-  このディレクトリにあるコードを実行します。パラメータは以下のとおりです。
+  PATH=$PATH:<任意のパス>/SCTK/bin
+```
 
-  ```bash
-  python evaluation.py \
-	--asr_train_config \
-	--asr_model_file \
-	--lm_train_config \
-	--lm_train_file \
+以下のコマンドを使って Argument が出力されれば OK です
 
+```bash
+sclite
+```
 
-	--config /opt/ml/processing/input/conf/decode_asr.yaml 
-	--batch_size 1 
-	--ngpu 0 
-	--data_path_and_name_and_type /opt/ml/processing/input/test/wav.scp,speech,sound
-	--asr_train_config config.yaml 
-	--asr_model_dir /opt/ml/processing/input/asr_model 
-	--lm_train_config config.yaml 
-	--lm_dir /opt/ml/processing/input/language_model 
-	--output_dir /opt/ml/processing/output/test 
-	--reference_dir /opt/ml/processing/input/test 
-	--score_output /opt/ml/processing/output/score
-  ```
+3. スクリプト
 
-# asr_inference
+このディレクトリにあるコードを実行します。パラメータは以下のとおりです。
+
+```bash
+python evaluation.py \
+	--asr_train_config /path/to/asr_conf.yml \
+	--asr_model_file  /path/to/asr_model.pth \
+	--lm_train_config /path/to/lm_conf.yml \
+	--lm_train_file /path/to/lm_model.pth \
+	--config /path/to/decode_asr.yaml \
+	--batch_size 1 \
+	--ngpu 0 \
+	--data_path_and_name_and_type /path/to/test/wav.scp,speech,sound
+	--output_dir /path/to/result
+	--reference_dir /path/to/result
+	--score_output /path/to/score
+```
+
+4. スコアの確認
+
+/path/to/score に結果のファイルがあるので、ご確認ください。
--- evaluate.py
+++ evaluate.py
@@ -1,44 +1,12 @@
 import argparse
 from pathlib import Path
 import logging
-import glob
-import re
-import tarfile
 
 from espnet2.bin.asr_inference import inference, get_parser
 import subprocess
 
 
 class FILE_IO:
-    @staticmethod
-    def replace(before: str, after: str, filepath: str):
-        """
-        replace any text in filepath
-        """
-        file_path = Path(filepath)
-        assert file_path.exists(), f"file:{filepath} is not exist. so cannot replace"
-        content = file_path.read_text()
-        content = content.replace(before, after)
-        file_path.write_text(content)
-
-    @staticmethod
-    def get_last_model_path(exp_dir: str) -> str:
-        """
-        Get the last model path in exp/
-        Args:
-            exp_dir(str): Directory path where the model file is located
-        Return:
-            str: last epoch model path string
-        """
-        max_num = -1
-        model_path = ""
-        for model in glob.glob(f"{exp_dir}/*epoch.pth"):
-            epoch_num = int(re.sub(r"\D*(\d*)epoch.pth", r"\1", model))
-            if max_num < epoch_num:
-                max_num = epoch_num
-                model_path = model
-        return model_path
-
     def make_trn_file(text_file_path: Path, dist_path: Path, basename: str):
         """
         make text file as <basename>.trn
@@ -65,37 +33,16 @@ class FILE_IO:
 
         return dist_file
 
-    def unzip_model(model_dir: Path, dist_dir: Path) -> Path:
-        """
-        unzip model.tar.gz
-        Args:
-            model_dir(str): directory that have model.tar.gz
-            dist_dir(str): Distination to unzip
-        Return:
-            Path
-        """
-
-        tar_gz_file = model_dir / "model.tar.gz"
-        assert tar_gz_file.exists(), "model.tar.gz file doesn't exist!!"
-
-        with tarfile.open(tar_gz_file) as tar:
-            tar.extractall(str(dist_dir))
-            parent = list(tar)[0].name
-        return dist_dir / parent
-
 
 def add_parser(parser: argparse.ArgumentParser):
-    # for decoder
-    parser.add_argument("--asr_model_dir", type=str, help="Directory path containing ASR model.tar.gz")
-    parser.add_argument(
-        "--lm_dir",
-        type=str,
-        help="Directory path containing LM model.tar.gz",
-    )
 
     # for scoerrer
-    parser.add_argument("--reference_dir", type=str, help="Directory that have reference text")
-    parser.add_argument("--score_output", type=str, help="output directory path for score ")
+    parser.add_argument(
+        "--reference_dir", type=str, help="Directory that have reference text"
+    )
+    parser.add_argument(
+        "--score_output", type=str, help="output directory path for score "
+    )
     return parser
 
 
@@ -105,37 +52,6 @@ def main(cmd=None):
     args = parser.parse_args(cmd)
     kwargs = vars(args)
     kwargs.pop("config", None)
-
-    # fix wav path
-    for i, (path, name, type) in enumerate(args.data_path_and_name_and_type):
-
-        scp_path = Path(path)
-        output_dir = Path(scp_path.parent.name)
-
-        output_dir.mkdir(exist_ok=True)
-        output_file = output_dir / "wav.scp"
-
-        content = scp_path.read_text()
-        content = content.replace("dump/raw/", "/opt/ml/processing/input/")
-        output_file.write_text(content)
-        args.data_path_and_name_and_type[i] = (str(output_file), name, type)
-
-    print(args.data_path_and_name_and_type)
-    # unzip model.tar.gz
-    distination = Path("/opt/ml/model")
-    # # ASR
-    asr_model_dir = FILE_IO.unzip_model(Path(args.asr_model_dir), distination / "asr_model")
-    args.asr_train_config = str(asr_model_dir / args.asr_train_config)
-    FILE_IO.replace("/opt/ml/input/data/", "/opt/ml/processing/input/", args.asr_train_config)
-    args.asr_model_file = FILE_IO.get_last_model_path(asr_model_dir)
-    logging.info(f"The name of the ASR model used for evaluation is {args.asr_model_file}.")
-
-    # # LM
-    lm_dir = FILE_IO.unzip_model(Path(args.lm_dir), distination / "language_model")
-    args.lm_train_config = str(lm_dir / args.lm_train_config)
-    FILE_IO.replace("/opt/ml/input/data/", "/opt/ml/processing/input/", args.lm_train_config)
-    args.lm_file = FILE_IO.get_last_model_path(lm_dir)
-    logging.info(f"The name of the ASR model used for evaluation is {args.lm_file}.")
 
     # inference
     inference(**kwargs)
@@ -168,7 +84,9 @@ def main(cmd=None):
     logging.info(" ".join(cmd))
 
     with open(output_score_file, "w") as wf:
-        proc = subprocess.Popen(cmd, encoding="utf-8", stdout=wf, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(
+            cmd, encoding="utf-8", stdout=wf, stderr=subprocess.PIPE
+        )
     _, stderr = proc.communicate()
     retcode = proc.returncode
 
